177b495917e3af37fa8b2f76c0927e348301c146
ericong18 <ericong18@gmail.com>
2018-03-10 16:49:39 -0800

    Added conditions for redirecting operators

--- src/command.cpp
+++ src/command.cpp
@@ -23,121 +23,181 @@ Command::Command(vector<string> v) {
 
 bool Command::evaluate() {
 	// cout << "Running COMMAND evaluate" << endl;
-	if (cmds.at(0) == "exit"){
+	if (cmds.at(0) == "exit") {
 		exit(0);
 	}
 	//check if its a test command
-	if (cmds.at(0) == "test" || cmds.at(0) == "["){
-		if (cmds.at(0) == "["){
+	if (cmds.at(0) == "test" || cmds.at(0) == "[") {
+		if (cmds.at(0) == "[") {
 			//check if brackets are valid
-			if (cmds.at(cmds.size()-1) != "]"){
+			if (cmds.at(cmds.size()-1) != "]") {
 				cout << "Invalid brackets" << endl;
 				return false;
 			}
 		}
 		//the different test commands
-		if (cmds.at(1) == "-e"){
-					struct stat info;
-					//check if file/directory is accessible
-					if(stat(const_cast<char*>(cmds.at(2).c_str()), &info) != 0){
-						cout << "Cannot access " << cmds.at(2) << endl;
-						cout << "(False)" << endl;
-						return false;
-					}
-					else{
-						cout << "(True)" << endl;
-						return true;
-					}
+		if (cmds.at(1) == "-e") {
+			struct stat info;
+			//check if file/directory is accessible
+			if (stat(const_cast<char*>(cmds.at(2).c_str()), &info) != 0) {
+				cout << "Cannot access " << cmds.at(2) << endl;
+				cout << "(False)" << endl;
+				return false;
 			}
+			else {
+				cout << "(True)" << endl;
+				return true;
+			}
+		}
 		else if (cmds.at(1) == "-f"){
-					struct stat info;
-					if(stat(const_cast<char*>(cmds.at(2).c_str()), &info) != 0){
-						cout << "Cannot access " << cmds.at(2) << endl;
-						cout << "(False)" << endl;
-						return false;
-					}
-					else if (info.st_mode & S_IFREG){
-						cout << "(True)" << endl;
-						return true;
-					}
-					else{
-						cout << "(False)" << endl;
-						return false;
-					}
+			struct stat info;
+			if (stat(const_cast<char*>(cmds.at(2).c_str()), &info) != 0){
+				cout << "Cannot access " << cmds.at(2) << endl;
+				cout << "(False)" << endl;
+				return false;
+			}
+			else if (info.st_mode & S_IFREG) {
+				cout << "(True)" << endl;
+				return true;
+			}
+			else {
+				cout << "(False)" << endl;
+				return false;
+			}
 		}
 		else if (cmds.at(1) == "-d"){
-					struct stat info;
-					if(stat(const_cast<char*>(cmds.at(2).c_str()), &info) != 0){
-						cout << "Cannot access " << cmds.at(2) << endl;
-						cout << "(False)" << endl;
-						return false;
-					}
-					else if (info.st_mode & S_IFDIR){
-						cout << "(True)" << endl;
-						return true;
-					}
-					else{
-						cout << "(False)" << endl;
-						return false;
-					}
+			struct stat info;
+			if (stat(const_cast<char*>(cmds.at(2).c_str()), &info) != 0){
+				cout << "Cannot access " << cmds.at(2) << endl;
+				cout << "(False)" << endl;
+				return false;
+			}
+			else if (info.st_mode & S_IFDIR){
+				cout << "(True)" << endl;
+				return true;
+			}
+			else {
+				cout << "(False)" << endl;
+				return false;
+			}
 		}
-		else{
-					struct stat info;
-					if(stat(const_cast<char*>(cmds.at(1).c_str()), &info) != 0){
-						cout << "cannot access " << cmds.at(1) << endl;
-						cout << "(False)" << endl;
-						return false;
-					}
-					else{
-						cout << "(True)" << endl;
-						return true;
-					}
+		else {
+			struct stat info;
+			if (stat(const_cast<char*>(cmds.at(1).c_str()), &info) != 0){
+				cout << "cannot access " << cmds.at(1) << endl;
+				cout << "(False)" << endl;
+				return false;
+			}
+			else {
+				cout << "(True)" << endl;
+				return true;
+			}
 		}
 		
 	}
 	//handles < operator
-	else if (cmds.find(cmds.begin(), cmds.end(), "<") != vector.end()){
+	else if (find(cmds.begin(), cmds.end(), "<") != cmds.end()){
+		cout << "Contains <" << endl;
 
+		vector<string> left;
+		vector<string> right;
+
+		int count = 0;
+		for (unsigned i = 0; i < cmds.size(); ++i) {
+			if (cmds.at(i) != "<") {
+				left.push_back(cmds.at(i));
+				++count;
+			}
+			else {
+				break;
+			}
+		}
+
+		++count;
+		for (unsigned i = count; i < cmds.size(); ++i) {
+			right.push_back(cmds.at(i));
+		}
 	}
 	//handles > and >> operator
-	else if (find(cmds.begin(), cmds.end(), ">") != vector.end() || find(cmds.begin(), cmds.end(), ">>") != vector.end()){
+	else if (find(cmds.begin(), cmds.end(), ">") != cmds.end() || find(cmds.begin(), cmds.end(), ">>") != cmds.end()){
+		cout << "Contains > or >>" << endl;
 
-	}
+		if (find(cmds.begin(), cmds.end(), ">") != cmds.end()) {
+			vector<string> left;
+			vector<string> right;
 
-	else{
-	unsigned arrSize = cmds.size() + 1;
-	char * args[arrSize]; // make a char pointer array of the same size as cmds vector
-    int status = 0;
-	// populate args array with commands in cmds vector
-	for (unsigned i = 0; i < arrSize - 1; ++i) {
-		args[i] = const_cast<char*>(cmds.at(i).c_str());
-	}
-	args[arrSize - 1] = NULL; // make last index NULL
+			int count = 0;
+			for (unsigned i = 0; i < cmds.size(); ++i) {
+				if (cmds.at(i) != ">") {
+					left.push_back(cmds.at(i));
+					++count;
+				}
+				else {
+					break;
+				}
+			}
 
-	pid_t pid = fork();
-	pid_t w;
-
-	if (pid < 0) { // if pid returns a negative value, then error
-		cout << "*** ERROR: forking child process failed\n" << endl;
-		exit(1);
-	}
-	else if (pid == 0) {
-		if (execvp(*args, args) < 0) { // if execvp returns, then error
-			cout << "*** ERROR: exec failed\n" << endl;
-            exit(1);
+			++count;
+			for (unsigned i = count; i < cmds.size(); ++i) {
+				right.push_back(cmds.at(i));
+			}
 		}
+		else {
+			vector<string> left;
+			vector<string> right;
+
+			int count = 0;
+			for (unsigned i = 0; i < cmds.size(); ++i) {
+				if (cmds.at(i) != ">>") {
+					left.push_back(cmds.at(i));
+					++count;
+				}
+				else {
+					break;
+				}
+			}
+
+			++count;
+			for (unsigned i = count; i < cmds.size(); ++i) {
+				right.push_back(cmds.at(i));
+			}
+		}
+
 	}
 	else {
-		w = waitpid(pid, &status, 0);
-		if (w == -1){
-			cout << "Issue with waitpid" << endl;
-			exit(EXIT_FAILURE);
+		unsigned arrSize = cmds.size() + 1;
+		char * args[arrSize]; // make a char pointer array of the same size as cmds vector
+	    int status = 0;
+		// populate args array with commands in cmds vector
+		for (unsigned i = 0; i < arrSize - 1; ++i) {
+			args[i] = const_cast<char*>(cmds.at(i).c_str());
 		}
-		if (WEXITSTATUS(status) == 0){
-			return true;
+		args[arrSize - 1] = NULL; // make last index NULL
+
+		pid_t pid = fork();
+		pid_t w;
+
+		if (pid < 0) { // if pid returns a negative value, then error
+			cout << "*** ERROR: forking child process failed\n" << endl;
+			exit(1);
+		}
+		else if (pid == 0) {
+			if (execvp(*args, args) < 0) { // if execvp returns, then error
+				cout << "*** ERROR: exec failed\n" << endl;
+	            exit(1);
+			}
+		}
+		else {
+			w = waitpid(pid, &status, 0);
+			if (w == -1){
+				cout << "Issue with waitpid" << endl;
+				exit(EXIT_FAILURE);
+			}
+			if (WEXITSTATUS(status) == 0){
+				return true;
+			}
 		}
 	}
-}
 
 	return false;
 }
